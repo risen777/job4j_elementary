f691d85d50ee33f94f42a3b924e00788975a54fc
risen777 <21risen@gmail.com>
2020-04-03 20:38:22 +0300

    Добавил таблицу умножения

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+/**
+ * Created by Sergey
+ */
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("1*" + i + "=" + 1 * i);
+
+        }
+
+
+    }
+}
