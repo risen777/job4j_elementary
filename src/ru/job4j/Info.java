58ce2b28a932e192db5920a8c3c73a8459bc32ef
risen777 <21risen@gmail.com>
2020-04-03 20:44:31 +0300

    Добавил дату

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * Created by Sergey
+ */
+public class Info {
+    public static void main(String[] args) {
+        System.out.println(Date.from(Instant.now()));
+    }
+
+}
