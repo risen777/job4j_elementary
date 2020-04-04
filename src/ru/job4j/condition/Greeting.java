c5567b5c3f56f038f4ca63cf4f31f7ca7c3ab06a
risen777 <21risen@gmail.com>
2020-04-04 21:57:35 +0300

    9.1. String[#267507]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+
+/**
+ * Created by Sergey
+ */
+public class Greeting {
+    public static void main(String[] args) {
+
+
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " But I am a newbie";
+        System.out.println(idea);
+        java.util.Calendar calendar = java.util.Calendar.getInstance(java.util.TimeZone.getDefault(), java.util.Locale.getDefault());
+        calendar.setTime(new java.util.Date());
+        int year = calendar.get(java.util.Calendar.YEAR);
+        System.out.println(year);
+
+    }
+}
