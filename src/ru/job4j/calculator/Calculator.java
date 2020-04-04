ad3223bae7ad6c807ccb9ba92ed817029ecd2f50
risen777 <21risen@gmail.com>
2020-04-04 13:18:51 +0300

    Добавил калькулятор

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+/**
+ * Created by Sergey
+ */
+public class Calculator {
+
+
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+
+
+    }
+}
