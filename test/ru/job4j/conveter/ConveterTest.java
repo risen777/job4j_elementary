2719e63f3029b7e98a7088bc2933fe6cb8f3961c
risen777 <21risen@gmail.com>
2020-04-05 00:50:20 +0300

    1. Модульные тесты.[#267553]

---
+++ test/ru/job4j/conveter/ConveterTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.conveter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+/**
+ * Created by Sergey
+ */
+public class ConveterTest {
+    private static final double DELTA = 1e-15;
+
+
+    @Test
+    public void whenConvert180RblThen2Euro57Cent() {
+        double in = 180;
+        double expected = 2.57;
+
+        double out = Conveter.rubleToEuro(in);
+        Assert.assertEquals(expected, out, DELTA);
+    }
+    @Test
+    public void whenConvert0RblThen0Euro() {
+        double in = 0;
+        double expected = 0;
+
+        double out = Conveter.rubleToEuro(in);
+        Assert.assertEquals(expected, out, DELTA);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        double in = 180;
+        double expected = 3.0;
+
+        double out = Conveter.rubleToDollar(in);
+        Assert.assertEquals(expected, out, DELTA);
+    }
+}
