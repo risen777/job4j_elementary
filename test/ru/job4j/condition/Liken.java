103439e289dd9fd0fab45f3f0914ae0911d4eab2
risen777 <21risen@gmail.com>
2020-04-05 21:35:22 +0300

    0. Операции сравнения[#267462]

---
+++ test/ru/job4j/condition/Liken.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+/**
+ * Created by Sergey
+ */
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println(result);
+
+        result = first < second;
+        System.out.println(result);
+
+        result = first == second;
+                System.out.println(result);
+    }
+}
