9f98c7afc3c534e08b3b2dd02c109eb2d1b54fc2
risen777 <21risen@gmail.com>
2020-04-10 21:45:02 +0300

    5.6. Простые числа.[#267539]

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -9,7 +9,7 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         boolean check = true;
-        for (int number = 0; number < finish; number++) {
+        for (int number = 2; number < finish; number++) {
             boolean rsl = CheckPrimeNumber.check(number);
             if (check == rsl) {
                 count++;
--- test/ru/job4j/loop/PrimeNumberTest.java
+++ test/ru/job4j/loop/PrimeNumberTest.java
@@ -13,21 +13,21 @@ public class PrimeNumberTest {
     public void when5() {
         PrimeNumber prime = new PrimeNumber();
         int count = prime.calc(5);
-        assertThat(count, is(3));
+        assertThat(count, is(2));
     }
 
     @Test
     public void when11() {
         PrimeNumber prime = new PrimeNumber();
         int count = prime.calc(11);
-        assertThat(count, is(5));
+        assertThat(count, is(4));
     }
 
     @Test
     public void when2() {
         PrimeNumber prime = new PrimeNumber();
         int count = prime.calc(2);
-        assertThat(count, is(1));
+        assertThat(count, is(0));
     }
 
 }
