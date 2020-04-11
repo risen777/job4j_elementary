b50ded505f833778506efd533e434c94c1d85b9b
risen777 <21risen@gmail.com>
2020-04-11 14:55:03 +0300

    5.6. Простые числа.[#267539]

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -8,10 +8,8 @@ public class PrimeNumber {
 
     public int calc(int finish) {
         int count = 0;
-        boolean check = true;
         for (int number = 2; number < finish; number++) {
-            boolean rsl = CheckPrimeNumber.check(number);
-            if (check == rsl) {
+            if (CheckPrimeNumber.check(number)) {
                 count++;
             }
         }
