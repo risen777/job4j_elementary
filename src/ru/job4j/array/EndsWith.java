37782453411351cc16d4c8f984404c263ab9f5c6
risen777 <21risen@gmail.com>
2020-04-15 00:33:23 +0300

    6.4.1. Слово заканчивается на ...[#267477]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -8,7 +8,6 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int i = 0; i < post.length; i++) {
-            System.out.println(word[word.length - post.length + i]);
             if (post[i] != word[word.length - post.length + i]) {
                 result = false;
                 break;
