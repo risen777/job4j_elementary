cbfd7dec4e46d11be59686700fae6a818f08eb29
risen777 <21risen@gmail.com>
2020-04-18 01:09:15 +0300

    6.6.2. Двухмерный массив. Таблица умножения.[#267472]

--- src/ru/job4j/array/BarleyBreak.java
+++ src/ru/job4j/array/BarleyBreak.java
@@ -6,21 +6,15 @@ package ru.job4j.array;
 public class BarleyBreak {
     public static void main(String[] args) {
         int[][] arr = new int[3][3];
-        arr[0][0] = 1;
-        arr[0][1] = 2;
-        arr[0][2] = 3;
-        arr[1][0] = 4;
-        arr[1][1] = 5;
-        arr[1][2] = 6;
-        arr[2][0] = 7;
-        arr[2][1] = 8;
-        arr[2][2] = 9;
-
+        int k = 1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = k;
+                k++;
                 System.out.print(arr[i][j] + "\t");
             }
             System.out.println();
         }
     }
-    }
+}
+
