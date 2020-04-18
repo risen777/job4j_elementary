874c8df5a36bd322a27130ffa18bebf4d5ed5d3e
risen777 <21risen@gmail.com>
2020-04-18 21:40:06 +0300

    6.7.1. Моно строка в матрице.[#267482]

---
+++ src/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ * Created by Sergey
+ */
+public class MatrixCheck {
+
+
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[row][j] != 'X') {
+                    result = false;
+                    break;
+                }
+            }
+
+        }
+        return result;
+
+    }
+}
---
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sergey
+ */
+public class MatrixCheckTest {
+
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasLastMonoHorizontalThenFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {'X', 'X', 'G'},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 2);
+        assertThat(result, is(false));
+    }
+
+}
