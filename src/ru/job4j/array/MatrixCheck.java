9439ed563bfb86ba3d8333755d1f525f60f0ac5f
risen777 <21risen@gmail.com>
2020-04-19 21:31:44 +0300

    6.7.1. Моно строка в матрице.[#267482]  6.7.2. Моно столбец в матрице.[#267483]   6.7.3. Массив из диагонали матрицы. [#267484]    6.7.4. Выигрышные комбинации в сокобан[#267475]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -8,63 +8,51 @@ public class MatrixCheck {
 
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[row][j] != 'X') {
-                    result = false;
-                    break;
-                }
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] != 'X') {
+                result = false;
+                break;
             }
-
         }
         return result;
-
     }
 
 
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][column] != 'X') {
-                    result = false;
-                    break;
-                }
-
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
             }
-
 
         }
         return result;
+
     }
+
 
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (i == j) {
-                    rsl[i] = board[i][j];
-                }
-
-            }
-
+            rsl[i] = board[i][i];
         }
         return rsl;
     }
 
+
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (i == j ) {
-                    if (monoHorizontal(board, i) || monoVertical(board, j)) {
+            if (board[i][i] == 'X') {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
 
-                        result = true;
-                    }
+                    result = true;
+                    break;
                 }
 
-            }
 
+            }
 
         }
         return result;
