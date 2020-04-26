3c72c5baccb7740344d304f6cf63ab8fb3a6bee6
risen777 <21risen@gmail.com>
2020-04-26 16:50:16 +0300

    1.1. Создание объекта[#267633]

---
+++ src/ru/job4j/oop/Dog.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+/**
+ * Created by Sergey
+ */
+public class Dog {
+
+
+    /**
+     * Собираем объект Dog
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+
+    }
+
+}
